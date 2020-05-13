a0df61b4440c0162fd48d3261cbc687138826f3d
Quentin Guilloteau <Quentin.Guilloteau@inria.fr>
2020-05-13 18:09:36 +0200

    setuptools

--- default.nix
+++ default.nix
@@ -8,8 +8,16 @@ in {
     name = "${app_name}";
     version = "${app_version}";
 
-    src = fetchTarball("https://github.com/GuilloteauQ/json2yaml/tarball/master");
+    # src = fetchTarball("https://github.com/GuilloteauQ/json2yaml/tarball/master");
+    src = fetchFromGitHub {
+      owner = "GuilloteauQ";
+      repo = "json2yaml";
+      rev = "5de4bc4b8aea55395db362f51238be63c7ee9a26";
+      sha256 = "001rapmzp08314c4y9birmfi6njgyvn7f95735qrd0443kfwhd19";
+    };
+    # src = ./.;
     propagatedBuildInputs = with python37Packages; [
+      setuptools
       pyaml
     ];
 
